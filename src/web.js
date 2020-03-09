c8403c0ce8006fa0ee1e32e1ea6e1c8d41da407b
Hoang Nguyen <50922013+catmandx@users.noreply.github.com>
2020-03-09 21:17:30 +0700

    Update web.js

--- src/web.js
+++ src/web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
-const bot = require("./app")
+
 var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
    res.send("Hello world!"+bot.r.ratelimitRemaining);
 });
 
 app.listen(port);
+
+const bot = require("./app")
 
