0079a8a6b83ae23c2938b063a1bed94561591ed2
Hoang Nguyen <50922013+catmandx@users.noreply.github.com>
2020-03-13 17:46:55 +0700

    .

--- src/test.js
+++ src/test.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
-const templ = require("./templates.js")
-require("util")
-var username = "/u/catmandx";
-var date = new Date();
+// require('dotenv').config();
+// const templ = require("./templates.js")
+// require("util")
+// var username = "/u/catmandx";
+// var date = new Date();
 
-// console.log(templ.getResolvedComment(username, date));
-var obj = JSON.parse(process.env.FIREBASE_ADMIN_CRED);
-console.log(obj)
+// // console.log(templ.getResolvedComment(username, date));
+// var obj = JSON.parse(process.env.FIREBASE_ADMIN_CRED);
+// console.log(obj)
 
-var admin = require('firebase-admin');
-const { brotliCompress } = require('zlib');
-admin.initializeApp({
-    credential:admin.credential.cert(obj)
-// ,
-//     databaseURL:"abc",
-//     databaseAuthVariableOverride:"abc"
-});
+// var admin = require('firebase-admin');
+// const { brotliCompress } = require('zlib');
+// admin.initializeApp({
+//     credential:admin.credential.cert(obj)
+// // ,
+// //     databaseURL:"abc",
+// //     databaseAuthVariableOverride:"abc"
+// });
 
-const bot = require("./app").initializeBot();
+// const bot = require("./app").initializeBot();
 
